a543ff1cfd7038dc8fa950c212e86179efefa214
gvalmana <gustavo.shotokai@gmail.com>
2023-12-26 14:48:51 -0500

    test

--- src/ConversionDeTipos.java
+++ src/ConversionDeTipos.java
@@ -30,5 +30,15 @@ public class ConversionDeTipos {
 
         otroRealStr = String.valueOf(1.23456e2f);
         System.out.println("otroRealStr = " + otroRealStr);
+
+        int i = 32767;
+        short s = (short)i;
+        System.out.println("s = " + s);
+        long l = i;
+        System.out.println("l = " + l);
+        char c = (char) i;
+        System.out.println("c = " + c);
+        float f = (float) i;
+        System.out.println("f = " + f);
     }
 }
---
+++ src/EjemploTestRendimientoConcat.java
@@ -0,0 +1,20 @@
+public class EjemploTestRendimientoConcat {
+    public static void main(String[] args) {
+        String a = "a";
+        String b = "b";
+        String c = a;
+        StringBuilder sb = new StringBuilder(a);
+        long inicio = System.currentTimeMillis();
+
+        for (int i=0; i<1000000;i++) {
+            //c = c.concat(a).concat(b).concat("\n"); // 500 => 1ms 1000 = 5ms
+            //c = c+a+b+"\n"; //500 => 19 1000 => 16ms
+            sb.append(a).append(b).append("\n"); // 500 => 0ms 1000 => 0ms
+        }
+
+        long fin = System.currentTimeMillis();
+        System.out.println(fin - inicio);
+        //System.out.println("c = " + c);
+        //System.out.println("sb = " + sb.toString());
+    }
+}

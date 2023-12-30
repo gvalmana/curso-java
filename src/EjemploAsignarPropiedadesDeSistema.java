9971aa56dda462f04b64cecce90cf6072f0fc0b4
gvalmana <gustavo.shotokai@gmail.com>
2023-12-30 12:24:01 -0500

    Clase terminada

---
+++ src/EjemploAsignarPropiedadesDeSistema.java
@@ -0,0 +1,27 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class EjemploAsignarPropiedadesDeSistema {
+    public static void main(String[] args) {
+        try {
+            FileInputStream archivo = new FileInputStream("src/config.properties");
+            Properties p = new Properties(System.getProperties());
+            p.load(archivo);
+            p.setProperty("mi.propiedad.personalizada","Mi valor guardado en el objeto properties");
+
+            System.setProperties(p);
+            Properties ps = System.getProperties();
+            System.out.println("ps.getProperty(\"mi.propiedad.personalizada\") = " + ps.getProperty("mi.propiedad.personalizada"));
+            System.out.println(System.getProperty("config.puerto.servidor"));
+            System.out.println(System.getProperty("config.autor.nombre"));
+            System.out.println(System.getProperty("config.autor.email"));
+            ps.list(System.out);
+        } catch (FileNotFoundException e) {
+            System.out.println("No existe el archivo " + e);
+        } catch (IOException e) {
+            System.out.println("Error al intentar cargar las configuraciones");
+        }
+    }
+}
